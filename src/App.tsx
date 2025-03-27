ad81406af261d97fb48d0bf3ecd0b7a19ef989d6
shimapiro <vjtoboku@gmail.com>
2025-03-27 18:37:17 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <div className="animate-fade-in">フェードイン</div>
+      <button className="animate-bounce">Click me!</button>
+    </>
+  );
+}
+
+export default App;
