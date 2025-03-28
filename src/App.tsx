64d4b7de63266fdf9cd4c37b70bbad52db88049f
shimapiro <vjtoboku@gmail.com>
2025-03-28 15:24:05 +0900

    修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginForm from "./components/LoginForm";
-import Dashboard from "./pages/Dashbord";
+import Dashboard from "./pages/Dashboard";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginForm />} />
-        <Route path="/dashbord" element={<Dashboard />} />
 
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<LoginForm />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Dashbord.tsx
+++
@@ -1,8 +0,0 @@
-
-const Dashboard = () => {
-  return (
-    <p className="p-6 text-xl font-bold text-center">ログイン成功！</p>
-  )
-}
-
-export default Dashboard
